0b9d35fa72c0485dd69ab4fecc90083790124701
Auto-Editor User <user@auto-editor.local>
2026-03-24 16:48:49 -0400

    chore: switch frontend-publish favicon to favicon.ico

--- scripts/generate-images.js
+++ scripts/generate-images.js
@@ -50,9 +50,7 @@ async function generate() {
     name: 'AutoEditor',
     short_name: 'AutoEditor',
     icons: [
-      { src: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { src: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { src: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+      { src: '/favicon.ico?v=20260324a', sizes: 'any', type: 'image/x-icon' }
     ],
     start_url: '/',
     display: 'standalone',
